4bf34c6998f95c7e50f8c43def05626c94e74f6c
larissarauj0 <larwarauj0@gmail.com>
2026-05-22 22:36:19 -0300

    feat: hero componente e animação de texto

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,40 @@
 "use client";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const words = ["inteligente", "estratégica", "competitiva", "precisa"];
 
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setCurrentIndex((currentIndex) => {
+        return (currentIndex + 1) % words.length;
+      });
+    }, 2000);
+
+    return () => clearInterval(intervalo);
+  }, []);
+
   return (
     <section className="min-h-screen min-w-screen bg-[#0a0f1e] flex flex-col items-start ">
       <h1 className="text-6xl font-bold text-[#f0f6ff] text-start ml-10 mt-20 w-1/3">
         Candidate-se de forma{" "}
-        <span className="text-[#4a9eff]"> inteligente. </span>
+        <AnimatePresence mode="wait">
+          <motion.span
+            key={currentIndex}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            className="text-[#4a9eff] block"
+            suppressHydrationWarning
+          >
+            {words[currentIndex]}
+            <span className="text-[#f0f6ff]">.</span>
+          </motion.span>
+        </AnimatePresence>
       </h1>
       <p className="text-[#8ba3c7] mt-6 ml-10 w-1/3">
         Do currículo à mensagem certa — tudo que você precisa para se destacar,
