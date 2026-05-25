4f63792d894d6ad4c486c0e8a3a6d948bbea36c9
larissarauj0 <larwarauj0@gmail.com>
2026-05-25 19:10:32 -0300

    :tentativa de stepper

--- app/components/Stepper.tsx
+++ app/components/Stepper.tsx
@@ -1,8 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import StepOne from "./steps/StepOne";
+import StepTwo from "./steps/StepTwo";
+import StepThree from "./steps/StepThree";
+
 interface StepperProps {
   id: string;
 }
 const Stepper = ({ id }: StepperProps) => {
-  return <div id={id}>{/* conteúdo */}</div>;
+  const [currentStep, setCurrentStep] = useState(1);
+  return (
+    <div id={id}>
+      <section className="flex flex-row items-center">
+        <div className="flex flex-col">
+          <span className="rounded-xl w-8 h-8 flex items-center justify-center bg-[#1a2a4a] border-2 border-[#2a4f7a]">
+            1{" "}
+          </span>
+          <span className="text-[#f0f6ff]"> Seus dados </span>
+        </div>
+        <div className="h-0.5 w-16 bg-[#2a4f7a]" />
+        <div className="flex flex-col">
+          <span className="rounded-xl w-8 h-8 flex items-center justify-center bg-[#1a2a4a] border-2 border-[#2a4f7a]">
+            2{" "}
+          </span>
+          <span className="text-[#f0f6ff]"> A vaga </span>
+        </div>
+        <div className="h-0.5 w-16 bg-[#2a4f7a]" />
+        <div className="flex flex-col items-center justify-center">
+          <span className="rounded-xl w-8 h-8 flex items-center justify-center bg-[#1a2a4a] border-2 border-[#2a4f7a]">
+            3{" "}
+          </span>
+          <span className="text-[#f0f6ff]"> Resultado </span>
+        </div>
+      </section>
+      <section>
+        {currentStep === 1 && <StepOne />}
+        {currentStep === 2 && <StepTwo />}
+        {currentStep === 3 && <StepThree />}
+      </section>
+    </div>
+  );
 };
 
 export default Stepper;
---
+++ app/components/steps/StepOne.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StepOne = () => {
+  return (
+    <div className='text-[#f0f6ff] flex items-center justify-center h-64'>
+      step one
+    </div>
+  )
+}
+
+export default StepOne
