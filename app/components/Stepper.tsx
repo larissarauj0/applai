d5cb0cf742daf359e2aba618fea84aef93150df8
larissarauj0 <larwarauj0@gmail.com>
2026-05-26 17:55:43 -0300

    :seleção do step one

--- app/components/Stepper.tsx
+++ app/components/Stepper.tsx
@@ -21,14 +21,6 @@ const Stepper = ({ id }: StepperProps) => {
 
   return (
     <div id={id}>
-      <div className="flex items-center justify-center h-64 text-[#f0f6ff]">
-        {stepper.flow.switch({
-          step1: () => <p>Seus dados</p>,
-          step2: () => <p>A vaga</p>,
-          step3: () => <p>O resultado</p>,
-        })}
-      </div>
-
       <section className="flex flex-row items-center justify-center gap-0 my-8">
         {steps.map((stepId, index) => (
           <React.Fragment key={stepId}>
@@ -68,11 +60,18 @@ const Stepper = ({ id }: StepperProps) => {
           </React.Fragment>
         ))}
       </section>
+      <div className="flex items-center justify-center h-64 text-[#f0f6ff]">
+        {stepper.flow.switch({
+          step1: () => <StepOne />,
+          step2: () => <StepTwo />,
+          step3: () => <StepThree />,
+        })}
+      </div>
 
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center m-2">
         {stepper.state.isLast ? (
           <button
-            className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl p-2 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300 flex"
+            className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl h-10 w-20 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300"
             onClick={() => stepper.navigation.reset()}
           >
             Resetar
@@ -80,14 +79,14 @@ const Stepper = ({ id }: StepperProps) => {
         ) : (
           <>
             <button
-              className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl p-2 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300"
+              className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl h-10 w-20 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300"
               onClick={() => stepper.navigation.prev()}
               disabled={stepper.state.isFirst}
             >
               Voltar
             </button>
             <button
-              className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl p-2 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300"
+              className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl h-10 w-20 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300"
               onClick={() => stepper.navigation.next()}
             >
               Próximo
--- app/components/steps/StepOne.tsx
+++ app/components/steps/StepOne.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
 
 const StepOne = () => {
+  const [statusStepOne, setStatusStepOne] = useState<
+    "none" | "upload" | "manual"
+  >("none");
   return (
-    <div className='text-[#f0f6ff] flex items-center justify-center h-64'>
-      step one
-    </div>
-  )
-}
+    <div>
+      {statusStepOne === "none" && (
+        <div className="flex flex-row items-center justify-center gap-4">
+          <button
+            className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl h-12 w-20 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300"
+            onClick={() => setStatusStepOne("upload")}
+          >
+            {" "}
+            Upload PDF{" "}
+          </button>
+          <button
+            className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl h-10 w-20 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300"
+            onClick={() => setStatusStepOne("manual")}
+          >
+            {" "}
+            Digitar cv{" "}
+          </button>
+        </div>
+      )}
 
-export default StepOne
+      {statusStepOne !== "none" && (
+        <div className="mt-4 flex flex-col items-center justify-center">
+          {statusStepOne === "upload" && <p>Área de upload</p>}
+
+          {statusStepOne === "manual" && <p>Formulário manual</p>}
+
+          <button
+            className="bg-[#1a2a4a] text-[#f0f6ff] rounded-xl h-10 w-20 cursor-pointer border border-[#21355c] hover:bg-[#4a9eff] hover:text-[#1a2a4a] transition duration-300 mt-3"
+            onClick={() => setStatusStepOne("none")}
+          >
+            Voltar
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StepOne;
